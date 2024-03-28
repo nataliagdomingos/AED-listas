6169de7ac0fe4a6d0cdf1d015a98474eb056745b
nataliagdomingos <164268964+nataliagdomingos@users.noreply.github.com>
2024-03-28 15:08:15 -0300

    Update lista1_1.cpp

--- POO/lista1_1.cpp
+++ POO/lista1_1.cpp
@@ -1,1 +1,61 @@
+#include <iostream>
 
+using namespace std;
+
+class agenda
+
+{
+    string nome;
+    string telefone;
+    public:
+        void setNome (string nome);
+        void setTelefone (string telefone);
+        string getNome();
+        string getTelefone();
+        void printAgenda ();
+        void printTelefone ();
+};   
+
+
+void agenda::setNome(string nome)
+{
+    this-> nome= nome;
+    
+};
+
+
+string agenda::getNome()
+{
+    return nome;
+};
+
+
+void agenda::setTelefone(string telefone)
+{
+    this -> telefone = telefone;
+};
+
+
+string agenda::getTelefone()
+{
+    return telefone;
+};
+
+void agenda::printTelefone(){
+    cout<<telefone<<"\n";
+}
+
+void agenda::printAgenda ()
+{
+    cout << nome << "," << telefone << "\n";
+}
+int main()
+{
+    agenda a;
+    a.setNome("Clara");
+    a.setTelefone("61984356273");
+    a.printAgenda();
+    a.printTelefone();
+    
+    return 0;
+}
