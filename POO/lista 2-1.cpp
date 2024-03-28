f59be69a789de77342fa7f1ce4e5f854084989b8
nataliagdomingos <164268964+nataliagdomingos@users.noreply.github.com>
2024-03-28 16:22:59 -0300

    Update lista 2-1.cpp

--- POO/lista 2-1.cpp
+++ POO/lista 2-1.cpp
@@ -1,1 +1,5 @@
-
+Questao 1
+a) Progamação Orientada a Objetos é um método de progamaçao que se baseia no obejto, onde é guardado dados e códigos 
+b) Classe é o que define as propiedades de um certo objeto 
+c) Instancia é quando você chama um obejeto para determinada classe
+d) É tudo que uma classe tem a favorecer 
